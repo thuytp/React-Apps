0b3156a7dae8d4df9b14339ecf5a334a421b476b
Thuy Tran <tranphuongthuy97@gmail.com>
2022-06-29 06:22:29 +0700

    Add UserList, ErrorModal - Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  function onAddUserHandler(uName, uAge) {
+    setUserList((prevUsers) => {
+      return [
+        ...prevUsers,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={onAddUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
