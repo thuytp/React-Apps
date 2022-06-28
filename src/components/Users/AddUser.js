4e904330cdc018294080aeb556c1127116751510
Thuy Tran <tranphuongthuy97@gmail.com>
2022-06-28 16:18:33 +0700

    User Project Initial

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,22 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+function AddUser(props) {
+  function addUserHandler(event) {
+    event.preventDefault();
+  }
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+}
+
+export default AddUser;
